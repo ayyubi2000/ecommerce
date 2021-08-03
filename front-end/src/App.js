15ad5e3cd7ee96fc431810901a1819a3df17d782
ayyubi2000 <72436671+ayyubi2000@users.noreply.github.com>
2021-08-03 11:49:37 +0500

    sayt front end

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button class="btn btn-primary">asdfas</button>
+      <button >asdfas</button>
+
+    </div>
+  );
+}
+
+export default App;
